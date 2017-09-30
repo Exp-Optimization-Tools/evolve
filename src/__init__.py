ea4123436e8f215fa8b62584c102a86450a4d2a6
Christian Kauten <Kautenja@users.noreply.github.com>
2017-09-30 15:59:48 -0500

    Update: src.__init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,13 @@
 """This package contains classes for running evolutionary algorithms."""
+# from typing import Callable
+# from .selection import *
+# from .procreation import *
+#
+#
+# def evolve(population_size: int,
+#            evaluation_func: Callable,
+#            procreator_func: Callable,
+#            iterations: int=4000):
+#     """
+#     """
+#     pass

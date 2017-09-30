7ed68c8d01c92935f771d55611ae1d8bd2215dd5
Christian Kauten <Kautenja@users.noreply.github.com>
2017-09-30 15:03:29 -0500

    Create RealCodedChromosome
    * left empty until I know how I want to flush it out

---
+++ src/population/realcoded_chromosome.py
@@ -0,0 +1,8 @@
+"""This module contains a class representing a real-coded chromosome."""
+from .chromosome import Chromosome
+
+
+class RealCodedChromosome(Chromosome):
+
+    def __init__(self):
+        raise ValueError('not implemented!')
---
+++ src/population/tests/test_realcoded_chromosome.py
@@ -0,0 +1,3 @@
+"""This module contains test code for the real-coded chromosome module."""
+import unittest
+from ..realcoded_chromosome import *

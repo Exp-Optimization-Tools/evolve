6db67bf8c97eea3e8e32ce503da564c1e8bad162
Christian Kauten <Kautenja@users.noreply.github.com>
2017-09-30 17:36:59 -0500

    Update ABCParentSelector

--- src/parent_selection/parent_selector.py
+++ src/parent_selection/parent_selector.py
@@ -1,5 +1,25 @@
 """This module contains the abstract base class for parent selection."""
+import abc
+from typing import Union
+from numpy import ndarray
 
 
 class ABCParentSelector:
-    pass
+    """An abstract base class outlining the API for parent selection"""
+
+    @abc.abstractmethod
+    def select(self, population: Union[list, ndarray]):
+        """
+        Select a subset from the population.
+
+        Args:
+            population: the list of Chromosomes to select from
+        """
+        if not isinstance(population, (list, ndarray)):
+            raise TypeError('population should be one of type: array, list')
+
+
+# explicitly export classes
+__all__ = [
+    'ABCParentSelector'
+]
--- src/parent_selection/tests/test_parent_selector.py
+++ src/parent_selection/tests/test_parent_selector.py
@@ -1,1 +1,43 @@
 """This module tests the parent_selector module."""
+import unittest
+from numpy import array
+from ..parent_selector import ABCParentSelector
+
+
+#
+# MARK: Abstract Base Class
+#
+
+
+class ParentSelectorTestCase(unittest.TestCase):
+    pass
+
+#
+# MARK: __init__()
+#
+
+class ShouldInstantiateABCParentSelector(ParentSelectorTestCase):
+    def runTest(self):
+        self.assertTrue(isinstance(ABCParentSelector(), ABCParentSelector))
+
+#
+# MARK: select(population)
+#
+
+class ShouldRaiseErrorOnInvalidPopulationWrongType(ParentSelectorTestCase):
+    def runTest(self):
+        sel = ABCParentSelector()
+        with self.assertRaises(TypeError):
+            sel.select('asdfasdfasdf')
+
+
+class ShouldNotRaiseErrorOnValidPopulationList(ParentSelectorTestCase):
+    def runTest(self):
+        sel = ABCParentSelector()
+        self.assertIsNone(sel.select([]))
+
+
+class ShouldNotRaiseErrorOnValidPopulationArray(ParentSelectorTestCase):
+    def runTest(self):
+        sel = ABCParentSelector()
+        self.assertIsNone(sel.select(array([])))

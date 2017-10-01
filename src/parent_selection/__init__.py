b1670001c30485a9bb34e0e281aa5c4cacf683f6
Christian Kauten <Kautenja@users.noreply.github.com>
2017-09-30 20:33:23 -0500

    Cleanup: parent_selection dunder init

--- src/parent_selection/__init__.py
+++ src/parent_selection/__init__.py
@@ -3,3 +3,6 @@ from .parent_selector import ABCParentSelector
 from .proportionate_selector import ProportionateSelector
 from .linear_rank_selector import LinearRankSelector
 from .tournament_selector import TournamentSelector
+# TODO: move the replacement boolean to an instance method to allow for more
+#       customization without impacting generality of the algorithm at the
+#       frontend of the API

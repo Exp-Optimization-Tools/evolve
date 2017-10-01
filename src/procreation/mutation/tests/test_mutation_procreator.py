b1f812a370bb921dacdfb0006edebd2b36546849
Christian Kauten <Kautenja@users.noreply.github.com>
2017-10-01 15:23:23 -0500

    Update MutationProcreatorABC
    * robustness testing and guard conditioning

--- src/procreation/mutation/tests/test_mutation_procreator.py
+++ src/procreation/mutation/tests/test_mutation_procreator.py
@@ -1,6 +1,6 @@
 """This module contains test code for the mutation_procreator module."""
 import unittest
-from population import BinaryChromosome
+from src.population import BinaryChromosome
 from ..mutation_procreator import *
 
 
@@ -24,7 +24,35 @@ class ShouldRaiseErrorOnMissingParameters(MutationProcreatorTestCase):
             mutator = MutationProcreatorABC()
 
 
-class ShouldCreateMutationProcreator(MutationProcreatorTestCase):
+class ShouldRaiseErrorOnInvalidMutationRateWrongType(MutationProcreatorTestCase):
+    def runTest(self):
+        with self.assertRaises(TypeError):
+            mutator = MutationProcreatorABC('asdf')
+
+
+class ShouldRaiseErrorOnNegativeMutationRate(MutationProcreatorTestCase):
+    def runTest(self):
+        with self.assertRaises(ValueError):
+            mutator = MutationProcreatorABC(-0.01)
+
+
+class ShouldRaiseErrorOnAboveOneMutationRate(MutationProcreatorTestCase):
+    def runTest(self):
+        with self.assertRaises(ValueError):
+            mutator = MutationProcreatorABC(1.01)
+
+
+class ShouldCreateMutationProcreatorRate0(MutationProcreatorTestCase):
+    def runTest(self):
+        mutator = MutationProcreatorABC(mutation_rate=0)
+
+
+class ShouldCreateMutationProcreatorRate1(MutationProcreatorTestCase):
+    def runTest(self):
+        mutator = MutationProcreatorABC(mutation_rate=1)
+
+
+class ShouldCreateMutationProcreatorRateArb(MutationProcreatorTestCase):
     def runTest(self):
         mutator = MutationProcreatorABC(mutation_rate=0.01)
 
@@ -34,14 +62,64 @@ class ShouldCreateMutationProcreator(MutationProcreatorTestCase):
 #
 
 
+class mutate_ShouldRaiseErrorOnMissingParameters(MutationProcreatorTestCase):
+    def runTest(self):
+        mutator = MutationProcreatorABC(mutation_rate=0.01)
+        with self.assertRaises(TypeError):
+            mutator.mutate()
+
+
+class mutate_ShouldRaiseErrorOnInvalidParamWrongType(MutationProcreatorTestCase):
+    def runTest(self):
+        mutator = MutationProcreatorABC(mutation_rate=0.01)
+        with self.assertRaises(TypeError):
+            mutator.mutate('asdf')
+
+
 def arb_eval(genes):
     return 0
 
 
-class ShouldNotMutate(MutationProcreatorTestCase):
+class mutate_ShouldNotMutateRate0(MutationProcreatorTestCase):
     def runTest(self):
         mutator = MutationProcreatorABC(mutation_rate=0)
         mutated = mutator.mutate(BinaryChromosome(size=5,
                                                   evaluate=arb_eval,
                                                   initial_state='zeros'))
         self.assertEqual([0,0,0,0,0], list(mutated.genes))
+
+
+class mutate_ShouldNotMutateRate0List(MutationProcreatorTestCase):
+    def runTest(self):
+        mutator = MutationProcreatorABC(mutation_rate=0)
+        chroms = [
+            BinaryChromosome(size=5, evaluate=arb_eval, initial_state='zeros'),
+            BinaryChromosome(size=5, evaluate=arb_eval, initial_state='zeros')
+        ]
+        mutated = mutator.mutate(chroms)
+        self.assertEqual([0,0,0,0,0], list(mutated[0].genes))
+        self.assertEqual([0,0,0,0,0], list(mutated[1].genes))
+
+
+class mutate_ShouldNotMutateRate1(MutationProcreatorTestCase):
+    def runTest(self):
+        mutator = MutationProcreatorABC(mutation_rate=1)
+        mutated = mutator.mutate(BinaryChromosome(size=5,
+                                                  evaluate=arb_eval,
+                                                  initial_state='zeros'))
+        self.assertEqual([0,0,0,0,0], list(mutated.genes))
+
+
+class mutate_ShouldRaiseErrorOnInvalidInPlaceWrongType(MutationProcreatorTestCase):
+    def runTest(self):
+        mutator = MutationProcreatorABC(mutation_rate=0.01)
+        chrom = BinaryChromosome(size=5, evaluate=arb_eval, initial_state='zeros')
+        with self.assertRaises(TypeError):
+            mutator.mutate(chrom, 'asdf')
+
+
+class mutate_ShouldMutateInPlace(MutationProcreatorTestCase):
+    def runTest(self):
+        mutator = MutationProcreatorABC(mutation_rate=0.01)
+        chrom = BinaryChromosome(size=5, evaluate=arb_eval, initial_state='zeros')
+        mutator.mutate(chrom, True)

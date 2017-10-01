88b547af763bb31c793f956d7d7714ba80e093f9
Christian Kauten <Kautenja@users.noreply.github.com>
2017-10-01 00:11:19 -0500

    Cleanup CrossoverProcreatorABC

--- src/procreation/crossover/crossover_procreator.py
+++ src/procreation/crossover/crossover_procreator.py
@@ -1,11 +1,15 @@
 """This module contains the crossover procreator abstract base class."""
+from abc import abstractmethod
 
 
 class CrossoverProcreatorABC:
     """This class is an abstract base class for crossover procreators."""
 
+    @abstractmethod
     def procreate(self, parents: list) -> list:
         """
+        Create a child from a collection of parents.
+        
         Args:
             parents: the list of parents to select from
             crossovers: the number of crossovers to perform

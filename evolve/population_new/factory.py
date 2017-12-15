0b0f15c4edf516880eff49859d7a6bb47338c932
Christian Kauten <Kautenja@users.noreply.github.com>
2017-12-15 16:44:19 -0600

    Update: ChromosomeFactory

--- evolve/population_new/factory.py
+++ evolve/population_new/factory.py
@@ -1,8 +1,38 @@
-"""An abstract factory for building random populations of chromosomes."""
+"""An abstract factory for generating populations of chromosomes."""
+from typing import Callable
 
 
 class ChromosomeFactory(object):
-    """A factory for generating random populations of chromosomes."""
+    """A factory for generating populations of chromosomes."""
 
-    def __init__(self):
-        """Initialize a new chromosome factory."""
+    def __init__(self,
+                 num_genes: int,
+                 decode: Callable,
+                 evaluate: Callable,
+                 initialization: str='random'):
+        """
+        Initialize a new chromosome factory.
+
+        Args:
+            num_genes: the number of genes in each chromosome
+            decode: a method that converts the gene set to a candidate solution
+            evaluate: a method that evaluates fitness of candidate solutions
+            initialization: the initial state for new chromosomes
+                (default 'random')
+        """
+        if not isinstance(num_genes, int):
+            raise TypeError('num_genes must be of type: int')
+        if not isinstance(decode, Callable):
+            raise TypeError('decode must be a callable method')
+        if not isinstance(evaluate, Callable):
+            raise TypeError('evaluate must be a callable method')
+        if not isinstance(initialization, (tuple, str)):
+            raise TypeError('initialization must be of type: tuple, str')
+        # type and value checks passed, assign instance members
+        self.num_genes = num_genes
+        self.decode = decode
+        self.evaluate = evaluate
+        self.initialization = initialization
+
+
+__all__ = ['ChromosomeFactory']
--- evolve/population_new/tests/test_factory.py
+++ evolve/population_new/tests/test_factory.py
@@ -1,2 +1,77 @@
 """Test cases for the ChromosomeFactory class."""
 from unittest import TestCase
+import numpy as np
+from ..factory import ChromosomeFactory
+# from .._chromosome import Chromosome
+
+
+def dummy_decode(genes: np.ndarray):
+    """A dummy decoder that returns the genotype"""
+    return genes
+
+
+def dummy_evaluate(phenotype: np.ndarray):
+    """A dummy evaluater that returns the sum of the CS"""
+    return phenotype.sum()
+
+
+#
+# MARK: __init__()
+#
+
+
+class ShouldRaiseErrorOnMissingChromosomeSize(TestCase):
+    def test(self):
+        with self.assertRaises(TypeError):
+            ChromosomeFactory()
+
+
+class ShouldRaiseErrorOnMissingDecode(TestCase):
+    def test(self):
+        with self.assertRaises(TypeError):
+            ChromosomeFactory(10)
+
+
+class ShouldRaiseErrorOnMissingEvaluate(TestCase):
+    def test(self):
+        with self.assertRaises(TypeError):
+            ChromosomeFactory(10, dummy_decode)
+
+
+class ShouldRaiseErrorOnInvalidTypeChromosomeSize(TestCase):
+    def test(self):
+        with self.assertRaises(TypeError):
+            ChromosomeFactory('asdf', dummy_decode, dummy_evaluate)
+
+
+class ShouldRaiseErrorOnInvalidTypeDecode(TestCase):
+    def test(self):
+        with self.assertRaises(TypeError):
+            ChromosomeFactory(10, 'asdf', dummy_evaluate)
+
+
+class ShouldRaiseErrorOnInvalidTypeEvaluate(TestCase):
+    def test(self):
+        with self.assertRaises(TypeError):
+            ChromosomeFactory(10, dummy_decode, 'asdf')
+
+
+class ShouldRaiseErrorOnInvalidTypeInitialization(TestCase):
+    def test(self):
+        with self.assertRaises(TypeError):
+            ChromosomeFactory(10, dummy_decode, dummy_evaluate, 6)
+
+
+class ShouldCreateChromosomeFactory(TestCase):
+    def test(self):
+        ChromosomeFactory(10, dummy_decode, dummy_evaluate)
+
+
+class ShouldCreateChromosomeFactoryInitializationString(TestCase):
+    def test(self):
+        ChromosomeFactory(10, dummy_decode, dummy_evaluate, 'random')
+
+
+class ShouldCreateChromosomeFactoryInitializationTuple(TestCase):
+    def test(self):
+        ChromosomeFactory(10, dummy_decode, dummy_evaluate, (0, 1))

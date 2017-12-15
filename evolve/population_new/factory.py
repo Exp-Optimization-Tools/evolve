1e9f4e3cbc6e9632898bdbc442d761009509ab63
Christian Kauten <Kautenja@users.noreply.github.com>
2017-12-15 17:11:52 -0600

    Update: factory

--- evolve/population_new/factory.py
+++ evolve/population_new/factory.py
@@ -1,5 +1,7 @@
 """An abstract factory for generating populations of chromosomes."""
-from typing import Callable
+from typing import Callable, List
+import numpy as np
+from ._chromosome import Chromosome
 
 
 class ChromosomeFactory(object):
@@ -20,6 +22,7 @@ class ChromosomeFactory(object):
             initialization: the initial state for new chromosomes
                 (default 'random')
         """
+        # validate types to ensure that no surprises happen later on
         if not isinstance(num_genes, int):
             raise TypeError('num_genes must be of type: int')
         if not isinstance(decode, Callable):
@@ -28,11 +31,48 @@ class ChromosomeFactory(object):
             raise TypeError('evaluate must be a callable method')
         if not isinstance(initialization, (tuple, str)):
             raise TypeError('initialization must be of type: tuple, str')
+        # validate values to really ensure no surprises happen later on
+        if num_genes <= 0:
+            raise ValueError('num_genes must be >= 0')
+        try:
+            random_phenotype = decode(np.random.random_sample(size=num_genes))
+        except Exception as err:
+            raise ValueError('decode should be a method: decode(genes: np.ndarray)')
+        try:
+            fitness = evaluate(random_phenotype)
+        except Exception as err:
+            raise ValueError('evaluate should be a method: evaluate(decode(genes: np.ndarray))')
+        if not isinstance(fitness, float):
+            raise ValueError('evaluate should return a float')
+
+        # TODO: validate that "initialization" is totally valid
+
         # type and value checks passed, assign instance members
         self.num_genes = num_genes
         self.decode = decode
         self.evaluate = evaluate
         self.initialization = initialization
+
+    @property
+    def next_individual(self) -> Chromosome:
+        """Create and return a new individual."""
+        # generate genes
+        # TODO: generate genes according to `initialization`
+        genes = None
+        # build a chromosome and return it
+        return Chromosome(genes, self.decode, self.evaluate)
+
+
+    # def population(self, size: int) -> List[Chromosome]:
+    #     """
+    #     Return a population of new chromosomes of given size.
+    #
+    #     Args:
+    #         size: the number of individuals to generate
+    #     """
+    #     if not isinstance(size, (int, float)) or size < 0:
+    #         raise ValueError('size must be a positive number')
+    #     return [self.next_individual for _ in range(int(size))]
 
 
 __all__ = ['ChromosomeFactory']
--- evolve/population_new/tests/test_factory.py
+++ evolve/population_new/tests/test_factory.py
@@ -44,16 +44,54 @@ class ShouldRaiseErrorOnInvalidTypeChromosomeSize(TestCase):
             ChromosomeFactory('asdf', dummy_decode, dummy_evaluate)
 
 
+class ShouldRaiseErrorOnInvalidValueChromosomeSizeNegative(TestCase):
+    def test(self):
+        with self.assertRaises(ValueError):
+            ChromosomeFactory(-1, dummy_decode, dummy_evaluate)
+
+
+class ShouldRaiseErrorOnInvalidValueChromosomeSizeZero(TestCase):
+    def test(self):
+        with self.assertRaises(ValueError):
+            ChromosomeFactory(0, dummy_decode, dummy_evaluate)
+
+
 class ShouldRaiseErrorOnInvalidTypeDecode(TestCase):
     def test(self):
         with self.assertRaises(TypeError):
             ChromosomeFactory(10, 'asdf', dummy_evaluate)
 
 
+def invalid_method():
+    pass
+
+
+def invalid_evaluate_return_type(genes):
+    return 'asdf'
+
+
+class ShouldRaiseErrorOnInvalidValueDecode(TestCase):
+    def test(self):
+        with self.assertRaises(ValueError):
+            ChromosomeFactory(10, invalid_method, dummy_evaluate)
+
+
 class ShouldRaiseErrorOnInvalidTypeEvaluate(TestCase):
     def test(self):
         with self.assertRaises(TypeError):
             ChromosomeFactory(10, dummy_decode, 'asdf')
+
+
+class ShouldRaiseErrorOnInvalidValueEvaluate(TestCase):
+    def test(self):
+        with self.assertRaises(ValueError):
+            ChromosomeFactory(10, dummy_decode, invalid_method)
+
+
+class ShouldRaiseErrorOnInvalidValueEvaluateReturn(TestCase):
+    def test(self):
+        with self.assertRaises(ValueError):
+            ChromosomeFactory(10, dummy_decode, invalid_evaluate_return_type)
 
 
 class ShouldRaiseErrorOnInvalidTypeInitialization(TestCase):
@@ -75,3 +113,32 @@ class ShouldCreateChromosomeFactoryInitializationString(TestCase):
 class ShouldCreateChromosomeFactoryInitializationTuple(TestCase):
     def test(self):
         ChromosomeFactory(10, dummy_decode, dummy_evaluate, (0, 1))
+
+
+#
+# MARK: next_individual
+#
+
+
+# class ShouldCreateChromosomeFromFactory(ChromosomeFactoryTestCase):
+#     def test(self):
+#         factory = ChromosomeFactory(5, dummy_decode, dummy_evaluate)
+#
+#
+# class ShouldCreateBinaryChromosomeFromFactory(ChromosomeFactoryTestCase):
+#     def test(self):
+#         factory = ChromosomeFactory(5, dummy_decode, dummy_evaluate, 'zeros')
+#
+#
+# class ShouldCreateBinaryChromosomeFromFactory(ChromosomeFactoryTestCase):
+#     def test(self):
+#         factory = ChromosomeFactory(5, dummy_decode, dummy_evaluate, 'ones')
+#
+#
+# class ShouldCreateRealCodedChromosomeFromFactory(ChromosomeFactoryTestCase):
+#     def test(self):
+#         factory = ChromosomeFactory(5, dummy_decode, dummy_evaluate, (1, 2))
+#         self.assertTrue(isinstance(factory.next_individual, RealCodedChromosome))
+#         for gene in factory.next_individual.genes:
+#             self.assertTrue(gene >= 1)
+#             self.assertTrue(gene <= 2)
